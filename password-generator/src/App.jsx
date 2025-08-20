61ad00bc1728dfe2713b9fa7ace3b6c0d814cb88
Hrittika321 <hrittika.pramanick@kreeti.com>
2025-08-20 18:23:08 +0530

    Pass-gen started

--- password-generator/src/App.jsx
+++ password-generator/src/App.jsx
@@ -1,13 +1,15 @@
-
-import './App.css'
+import "./App.css";
 
 function App() {
-
   return (
     <>
-      
+      <div className="flex justify-center items-center ">
+        <div className="flex justify-center items-center h-30 w-150 bg-amber-700 m-2 text-3xl mt-20">
+        <textarea name="password" id="password" className="flex justify-centre items-center bg-amber-100 text-amber-950 text-2xl w-140 h-10 rounded-2xl "></textarea>
+      </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
